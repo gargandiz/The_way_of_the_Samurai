c0c8f445fd098a54e08a7b885f434cc26c6e7ebc
gargandiz <gargandzolla10@gmail.com>
2025-12-29 12:23:52 +0200

    lesson_13

---
+++ src/components/Profile.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Profile = () => {
+    return <div className='content'>
+        <div>
+            <img src="https://i.ytimg.com/vi/V3V33QRm7zk/maxresdefault.jpg" alt=""/>
+        </div>
+        <div>ava+description</div>
+        <div>New posts</div>
+        <div>
+            <div>Post 1</div>
+            <div>Post 2</div>
+            <div>Post 3</div>
+            <div>Post 4</div>
+        </div>
+    </div>
+}
+
+export default Profile;
