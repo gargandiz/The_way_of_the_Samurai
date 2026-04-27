04b97f999fbec0a4e81d3809edb534ff526fe017
gargandiz <gargandzolla10@gmail.com>
2026-04-27 19:14:19 +0300

    lessom_31

--- src/components/Dialogs/Message/Message.jsx
+++ src/components/Dialogs/Message/Message.jsx
@@ -2,9 +2,28 @@ import React from "react";
 import s from "./../Dialogs.module.css"
 
 const Message = (props) => {
-    return <div className={s.message}>
-        {props.message}
-    </div>
+
+    let newMessageElement = React.createRef();
+
+    let addMessage = () => {
+        let text = newMessageElement.current.value;
+        alert(text)
+    }
+
+    return (
+        <div className={s.message}>
+            <div>
+                {props.message}
+            </div>
+            <div>
+                <textarea ref={newMessageElement}></textarea>
+            </div>
+            <div>
+                <button onClick={addMessage}>Add message</button>
+            </div>
+        </div>
+
+    )
 }
 
 export default Message;
